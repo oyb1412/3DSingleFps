ba14b7663fa13172ded9f252371f6eb749dfa9cf
oyb1412 <dhdbsqja@gmail.com>
2024-04-26 18:07:11 +0900

    2024/04/26
    dsa

--- Assets/02.Scripts/Controller/Weapon/Base/WeaponController.cs
+++ Assets/02.Scripts/Controller/Weapon/Base/WeaponController.cs
@@ -158,6 +158,7 @@ namespace Base {
             }
         }
 
+        //todo
         [PunRPC]
         public void RPC_TakeDamage(int damage, int attackerHandle, int myHandle, bool headShot) {
             if (PV.OwnerActorNr != attackerHandle)
@@ -165,7 +166,7 @@ namespace Base {
 
             UnitBase attacker = Util.FindPlayerByActorNumber(attackerHandle);
             UnitBase me = Util.FindPlayerByActorNumber(myHandle);
-            me.TakeDamage(damage, attacker.transform, me.transform, headShot);
+            me?.TakeDamage(damage, attacker.transform, me.transform, headShot);
         }
 
         public virtual void Shot() {
